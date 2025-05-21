16eca4cb439b655bc2ef4d2d9e5f25903ba30b2b
sahil soni <sonisahil9905@gmail.com>
2025-05-21 20:45:53 +0530

    gdf

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // ✅ Ensure this matches what Vercel expects
-    emptyOutDir: true, // Optional: clears dist folder before each build
-  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
